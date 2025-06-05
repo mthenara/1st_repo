31a2f56066ab5d78d91a7ca267f0f3b59b662e90
mthenara <mthenara@gmail.com>
2025-06-05 16:05:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Website berhasil dimuat !");
+function sayHello() {
+  const name = document.getElementById("nameInput").value.trim();
+  const greeting = document.getElementById("greeting");
+
+  if (name === "") {
+    greeting.textContent = "Silakan masukkan nama terlebih dahulu.";
+  } else {
+    greeting.textContent = `Halo, ${name}! Selamat datang di website ini.`;
+  }
+}
